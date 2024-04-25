a8d244af8921b55e97071fc9a9b465a4323c6d34
Wissam Farhat <WissamFarhat@angrymonkeyagency.com>
2024-04-25 21:47:40 +0300

    update css for responsive

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component,Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
 
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductsComponent {
+  isFilterOpen :boolean =false;
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+
+  ToogleMenu(){
+    this.isFilterOpen=!this.isFilterOpen;
+    this.document.documentElement.classList.toggle("_noscroll");
+  }
 }
